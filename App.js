6058d5d0f523ba14f82e2e740d8e09826be0385f
Khalil G <iam.khalil.guezguez@gmail.com>
2022-07-15 09:04:00 +0100

    scan QR

--- App.js
+++ App.js
@@ -1,21 +1,130 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { View, Dimensions, Text } from "react-native";
+import QRCodeScanner from "react-native-qrcode-scanner";
+// import Icon from "react-native-vector-icons/Ionicons";
+import * as Animatable from "react-native-animatable";
+import Vector from "./assets/Vector.svg";
+
+const SCREEN_HEIGHT = Dimensions.get("window").height;
+const SCREEN_WIDTH = Dimensions.get("window").width;
+
+console.disableYellowBox = true;
+
+class QrCodeCamera extends Component {
+  onSuccess(e) {
+    alert(e);
+  }
+
+  makeSlideOutTranslation(translationType, fromValue) {
+    return {
+      from: {
+        [translationType]: SCREEN_WIDTH * -0.18,
+      },
+      to: {
+        [translationType]: fromValue,
+      },
+    };
+  }
+
+  render() {
+    return (
+      <QRCodeScanner
+        showMarker
+        onRead={this.onSuccess.bind(this)}
+        cameraStyle={{ height: SCREEN_HEIGHT }}
+        customMarker={
+          <View style={styles.rectangleContainer}>
+            <View style={styles.topOverlay}>
+              <Text style={{ fontSize: 30, color: "white" }}>
+                veuillez scanner le code QR
+              </Text>
+            </View>
+
+            <View style={{ flexDirection: "row" }}>
+              <View style={styles.leftAndRightOverlay} />
+
+              <View style={styles.rectangle}>
+                <Animatable.View
+                  style={styles.scanBar}
+                  direction="alternate-reverse"
+                  iterationCount="infinite"
+                  duration={1500}
+                  easing="linear"
+                  animation={this.makeSlideOutTranslation(
+                    "translateY",
+                    SCREEN_HEIGHT * 0.1
+                  )}
+                />
+              </View>
+
+              <View style={styles.leftAndRightOverlay} />
+            </View>
+
+            <View style={styles.bottomOverlay} />
+          </View>
+        }
+      />
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
+const overlayColor = "rgba(0,0,0,0.5)"; // this gives us a black color with a 50% transparency
+
+const rectDimensions = SCREEN_WIDTH * 0.65; // this is equivalent to 255 from a 393 device width
+const rectBorderWidth = SCREEN_WIDTH * 0.005; // this is equivalent to 2 from a 393 device width
+const rectBorderColor = "transparent";
+
+const scanBarWidth = SCREEN_WIDTH * 0.46; // this is equivalent to 180 from a 393 device width
+const scanBarHeight = SCREEN_WIDTH * 0.0025; //this is equivalent to 1 from a 393 device width
+const scanBarColor = "orange";
+
+const styles = {
+  rectangleContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "transparent",
   },
-});
+
+  rectangle: {
+    height: rectDimensions,
+    width: rectDimensions,
+    borderWidth: rectBorderWidth,
+    borderColor: rectBorderColor,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "transparent",
+  },
+
+  topOverlay: {
+    flex: 1,
+    height: SCREEN_WIDTH,
+    width: SCREEN_WIDTH,
+    backgroundColor: overlayColor,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  bottomOverlay: {
+    flex: 1,
+    height: SCREEN_WIDTH,
+    width: SCREEN_WIDTH,
+    backgroundColor: overlayColor,
+    paddingBottom: SCREEN_WIDTH * 0.25,
+  },
+
+  leftAndRightOverlay: {
+    height: SCREEN_WIDTH * 0.65,
+    width: SCREEN_WIDTH,
+    backgroundColor: overlayColor,
+  },
+
+  scanBar: {
+    width: scanBarWidth,
+    height: scanBarHeight,
+    backgroundColor: scanBarColor,
+  },
+};
+
+export default QrCodeCamera;
